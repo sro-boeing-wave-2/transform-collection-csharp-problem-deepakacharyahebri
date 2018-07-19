5974a1145fe67a6aa0e54b7e19aec74c5efb109b
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-19 21:10:41 +0530

    codes updated

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,20 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public static List<int> Map(this int[] NumberArray, Func<int,int> perform)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            /*Some Useful Links:
+             * https://www.codeproject.com/Tips/771746/Uses-of-Func-in-Csharp
+             * https://msdn.microsoft.com/en-us/library/bb534966(v=vs.110).aspx
+             * https://codeblog.jonskeet.uk/2011/01/05/reimplementing-linq-to-objects-part-26b-orderby-descending-thenby-descending/
+             */
+            List<int> Numbers = new List<int>();
+            foreach(int num in NumberArray)
+            {
+                Numbers.Add(perform(num));
+            }
+            return Numbers;
         }
     }
 }
