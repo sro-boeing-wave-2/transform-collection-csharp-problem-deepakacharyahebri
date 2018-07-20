e87882cce87845e6c329cfe191347884b2401ecf
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-20 12:21:24 +0530

    codes updated

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -9,7 +9,7 @@ namespace Transform
         public static List<int> Map(this int[] NumberArray, Func<int,int> perform)
         {
             //throw new NotImplementedException();
-            /*Some Useful Links:
+            /*Some Useful Links :
              * https://www.codeproject.com/Tips/771746/Uses-of-Func-in-Csharp
              * https://msdn.microsoft.com/en-us/library/bb534966(v=vs.110).aspx
              * https://codeblog.jonskeet.uk/2011/01/05/reimplementing-linq-to-objects-part-26b-orderby-descending-thenby-descending/
